d084b2fa63b05ed8018b04bffea599ede31dfdd3
devpycofa <62728403+devpycofa@users.noreply.github.com>
2024-11-05 15:47:06 +0100

    upgraded turborepo and using another component

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@repo/ui/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@repo/ui/components/ui/card";
 
 export default function Page() {
   return (
     <main>
       <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+        </CardHeader>
+      </Card>
     </main>
   );
 }
