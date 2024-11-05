8e9ba1f8e973ce5a8fc13f34bd26653d747ff0e8
devpycofa <62728403+devpycofa@users.noreply.github.com>
2024-11-05 16:07:02 +0100

    expo init

---
+++ apps/native/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Page = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello World ?????</Text>
+    </View>
+  );
+};
+
+export default Page;
